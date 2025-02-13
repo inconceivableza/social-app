82759056b64fe372d4c9f433bb59bcb21edcd1f6
David Fraser <davidf@inconceivable.co.za>
2025-02-13 11:19:35 +0000

    Disable custom hosting providers by commenting out the relevant UI

--- src/screens/Login/LoginForm.tsx
+++ src/screens/Login/LoginForm.tsx
@@ -171,6 +171,7 @@ export const LoginForm = ({
 
   return (
     <FormContainer testID="loginForm" titleText={<Trans>Sign in</Trans>}>
+      {/*
       <View>
         <TextField.LabelText>
           <Trans>Hosting provider</Trans>
@@ -181,6 +182,7 @@ export const LoginForm = ({
           onOpenDialog={onPressSelectService}
         />
       </View>
+      */}
       <View>
         <TextField.LabelText>
           <Trans>Account</Trans>
