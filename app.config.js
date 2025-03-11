e1782f153928891137578805f580ef9020e71ba4
David Fraser <davidf@inconceivable.co.za>
2025-03-11 13:37:42 +0200

    Disabled AppClip as the iOS build is not somehow loading the correct entitlements and so has profile complaints

--- app.config.js
+++ app.config.js
@@ -22,8 +22,9 @@ module.exports = function (config) {
   const ASSOCIATED_DOMAINS = [
     'applinks:bsky.app',
     'applinks:staging.bsky.app',
-    'appclips:bsky.app',
-    'appclips:go.bsky.app', // Allows App Clip to work when scanning QR codes
+    // Disabling AppClip as build failing
+    // 'appclips:bsky.app',
+    // 'appclips:go.bsky.app', // Allows App Clip to work when scanning QR codes
     // When testing local services, enter an ngrok (et al) domain here. It must use a standard HTTP/HTTPS port.
     ...(IS_DEV || IS_TESTFLIGHT ? [] : []),
   ]
@@ -239,7 +240,7 @@ module.exports = function (config) {
             networkInstrumentation: true,
           },
         ],
-        './plugins/starterPackAppClipExtension/withStarterPackAppClip.js',
+        // './plugins/starterPackAppClipExtension/withStarterPackAppClip.js',
         './plugins/withGradleJVMHeapSizeIncrease.js',
         './plugins/withAndroidManifestPlugin.js',
         './plugins/withAndroidManifestFCMIconPlugin.js',
@@ -396,10 +397,10 @@ module.exports = function (config) {
                       ],
                     },
                   },
-                  {
+                  /* {
                     targetName: 'BlueskyClip',
                     bundleIdentifier: 'xyz.blueskyweb.app.AppClip',
-                  },
+                  }, */
                 ],
               },
             },
