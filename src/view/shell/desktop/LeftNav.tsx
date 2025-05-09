4e23c7909226a05d417e0cf2c5d8cd79377d6256
David Fraser <davidf@inconceivable.co.za>
2025-05-09 11:30:39 +0200

    Added a flag for disabling chat, set to true
    (cherry picked from commit 6c447188ba877282bca2ef4d232707a6a8902ff2)
    (cherry picked from commit dadef6aaae0f9c55f733b9788c260d60b4e43f09)
    (cherry picked from commit f744b730d076b7b910a6ae8de80791f358ef8d1b)

--- src/view/shell/desktop/LeftNav.tsx
+++ src/view/shell/desktop/LeftNav.tsx
@@ -6,6 +6,7 @@ import {useLingui} from '@lingui/react'
 import {useNavigation, useNavigationState} from '@react-navigation/native'
 
 import {useActorStatus} from '#/lib/actor-status'
+import {CHAT_DISABLED} from '#/lib/constants'
 import {useAccountSwitcher} from '#/lib/hooks/useAccountSwitcher'
 import {useOpenComposer} from '#/lib/hooks/useOpenComposer'
 import {usePalette} from '#/lib/hooks/usePalette'
@@ -654,7 +655,7 @@ export function DesktopLeftNav() {
             }
             label={_(msg`Notifications`)}
           />
-          <ChatNavItem />
+          {!CHAT_DISABLED && <ChatNavItem />}
           <NavItem
             href="/feeds"
             icon={
