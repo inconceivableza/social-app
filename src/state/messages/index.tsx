9c6cd3b6c76c7984cbb4346dc55b979aefd8068c
David Fraser <davidf@inconceivable.co.za>
2025-06-06 15:25:25 +0200

    Also disable the message bus, so it doesn't get instantiated
    (cherry picked from commit b8fc0aed5e5d6d76533bb9c698341813045b23f8)
    (cherry picked from commit 6cdf5cd35208284aa9c260533a471d0a8977a0f1)

--- src/state/messages/index.tsx
+++ src/state/messages/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import type React from 'react'
 
+import {CHAT_DISABLED} from '#/lib/constants'
 import {CurrentConvoIdProvider} from '#/state/messages/current-convo-id'
 import {MessagesEventBusProvider} from '#/state/messages/events'
 import {ListConvosProvider} from '#/state/queries/messages/list-conversations'
 import {MessageDraftsProvider} from './message-drafts'
 
 export function MessagesProvider({children}: {children: React.ReactNode}) {
-  return (
+  return !CHAT_DISABLED ? (
     <CurrentConvoIdProvider>
       <MessageDraftsProvider>
         <MessagesEventBusProvider>
@@ -14,5 +15,7 @@ export function MessagesProvider({children}: {children: React.ReactNode}) {
         </MessagesEventBusProvider>
       </MessageDraftsProvider>
     </CurrentConvoIdProvider>
+  ) : (
+    {children}
   )
 }
