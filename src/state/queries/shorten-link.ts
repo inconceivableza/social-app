3d1f156450dd1bca16bd0f6c8f67e371bd11522e
David Fraser <davidf@inconceivable.co.za>
2025-08-19 16:17:43 +0200

    Use environment variables in-place in strings for domain names These would mostly benefit from using a constant instead (some are now using constants)
    (cherry picked from commit ab065e0948080470ff61f35109f952491da8e216)

--- src/state/queries/shorten-link.ts
+++ src/state/queries/shorten-link.ts
@@ -3,15 +3,18 @@ import {logger} from '#/logger'
 export function useShortenLink() {
   return async (inputUrl: string): Promise<{url: string}> => {
     const url = new URL(inputUrl)
-    const res = await fetch('https://go.foodios.app/link', {
-      method: 'POST',
-      body: JSON.stringify({
-        path: url.pathname,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
+    const res = await fetch(
+      `https://${process.env.LINK_HOST || 'go.bsky.app'}/link`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          path: url.pathname,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    })
+    )
 
     if (!res.ok) {
       logger.error('Failed to shorten link', {safeMessage: res.status})
