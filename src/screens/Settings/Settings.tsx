de0c367ed68e8799d1fa2f84cd645378f11d2724
David Fraser <davidf@inconceivable.co.za>
2025-08-27 12:02:04 +0200

    Some more UI cleanup Include bluesky server by default as it can be useful to test against But not bluesky_staging which is really just there to preserve internal bluesky testing info
    (cherry picked from commit 57bdaf79ca56ffe7e18e1e850291e12cd082ba64)

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -587,7 +587,7 @@ function ServerDomains() {
           <Trans>Reset Stored Environment Config</Trans>
         </SettingsList.ItemText>
       </SettingsList.PressableItem>
-      {['bluesky'].concat(builtinConfigNames).map(configName => {
+      {builtinConfigNames.map(configName => {
         const thisEnvConfig = DOMAIN_ENVCONFIGS[configName]
         if (thisEnvConfig) {
           const socialAppHost = thisEnvConfig.SOCIAL_APP_HOST
