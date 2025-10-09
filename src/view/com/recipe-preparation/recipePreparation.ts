526f8ede17eb08702ea630212ef14021933c2983
NevilleVH <1nevillevh@gmail.com>
2025-10-09 15:14:21 +0200

    Define hook for managing recipe preparation state

---
+++ src/view/com/recipe-preparation/recipePreparation.ts
@@ -0,0 +1,66 @@
+import { cloneDeep } from "lodash"
+import { PreparationState } from "./types"
+import { useSession } from "#/state/session"
+import { account, useStorage } from "#/storage"
+import { AppFoodiosFeedDefs, AppFoodiosFeedRecipeRevision } from "@atproto/api"
+import { useCallback, useMemo } from "react"
+
+function initPreparationState(record: AppFoodiosFeedRecipeRevision.Record): PreparationState {
+    return {
+        ingredients: record.ingredients.map(() => ({ checked: false })),
+        instructionSections: record.instructionSections.map(({ instructions }) => instructions.map(() => ({ checked: false })))
+    }
+}
+
+export function usePreparationState(revision: AppFoodiosFeedDefs.RecipeRevisionView) {
+    const { currentAccount } = useSession()
+    const [storedState, setStoredState] = useStorage(account,
+        [currentAccount?.did ?? 'pwi', "recipePreparations"])
+
+    const preparationState = useMemo(() =>
+        storedState?.[revision.selectedRevisionUri] ??
+        initPreparationState(revision.revisionContent)
+        , [storedState, revision])
+
+    const dispatch = useCallback((action: PreparationAction) => {
+        // Possibility of out of order updates here
+        const updatedState = {
+            ...storedState, [revision.selectedRevisionUri]:
+                preparationReducer(preparationState, action)
+        }
+        setStoredState(updatedState)
+    }, [preparationState, storedState, revision.selectedRevisionUri])
+    return [preparationState, dispatch] as const
+
+}
+
+type Action<Type extends string, Payload extends {}> = {
+    type: Type
+} & Payload
+
+export type PreparationAction = {
+    type: "toggle_ingredient"
+    idx: number
+} | Action<"toggle_instruction", {
+    sectionIdx: number
+    instructionIdx: number
+}>
+
+export function preparationReducer(state: PreparationState, action: PreparationAction): PreparationState {
+    state = cloneDeep(state)
+    switch (action.type) {
+        case "toggle_ingredient": {
+            const ingredient = state.ingredients.at(action.idx)
+            if (!ingredient) return state
+            ingredient.checked = !ingredient.checked
+            return state
+        }
+        case "toggle_instruction": {
+            const section = state.instructionSections.at(action.sectionIdx)
+            const instruction = section?.at(action.instructionIdx)
+            if (!instruction) return state
+            instruction.checked = !instruction.checked
+            return state
+        }
+    }
+}
