d0150b5b7805a7b892e581ea41bea585670cf0f3
David Fraser <davidf@inconceivable.co.za>
2025-11-26 15:04:12 +0200

    Find the env-content files under the atproto repository where they are owned
    (cherry picked from commit 5ab094a054be82f62017afb7162640a144adb8a8)

--- app.config.js
+++ app.config.js
@@ -105,7 +105,9 @@ module.exports = function (_config) {
   const envContentConfigs = Object.fromEntries(
     Object.entries(envContentFilenames).map(
       ([profileName, contentBasename]) => {
-        const contentFilename = findExisting(path.join('conf', contentBasename))
+        const contentFilename = findExisting(
+          path.join(defaultEnvPath, 'submodules', 'atproto', contentBasename),
+        )
         const contentSource = contentFilename
           ? fs.readFileSync(contentFilename).toString('utf-8')
           : '{}'
