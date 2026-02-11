28dacdf5bb834b6cd8fd1171f8dabd33f6e766b4
James Fraser <84665984+jamesfraser007@users.noreply.github.com>
2026-02-11 15:47:31 +0200

    Changed relative path

--- scripts/get-social-app-android-build-properties.js
+++ scripts/get-social-app-android-build-properties.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var app_config_script = require('../repos/social-app/app.config.js')
+var app_config_script = require('../app.config.js')
 var app_config = app_config_script()
 var plugins_config = (app_config.expo || {}).plugins || []
 var build_configs = plugins_config.filter(
